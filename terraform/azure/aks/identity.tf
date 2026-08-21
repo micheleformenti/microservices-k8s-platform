6e22a8f9747e925578daa770177565f87714ca31
Michele Formenti <michele.formenti@proton.me>
2026-08-21 13:49:51 +0200

    Migrate AKS identity ownership

--- terraform/azure/aks/identity.tf
+++ terraform/azure/aks/identity.tf
@@ -1,4 +1,17 @@
+resource "azurerm_user_assigned_identity" "aks_imported" {
+  name                = "id-${var.project_name}-aks"
+  location            = data.azurerm_resource_group.project.location
+  resource_group_name = data.azurerm_resource_group.project.name
+
+  tags = local.common_tags
+}
+
 data "azurerm_user_assigned_identity" "aks" {
   name                = "id-${var.project_name}-aks"
   resource_group_name = data.azurerm_resource_group.project.name
+}
+
+import {
+  to = azurerm_user_assigned_identity.aks_imported
+  id = "/subscriptions/${var.subscription_id}/resourceGroups/rg-${var.project_name}/providers/Microsoft.ManagedIdentity/userAssignedIdentities/id-${var.project_name}-aks"
 }
--- terraform/azure/bootstrap/rbac.tf
+++ terraform/azure/bootstrap/rbac.tf
@@ -1,3 +1,8 @@
+data "azurerm_role_definition" "network_contributor" {
+  name  = "Network Contributor"
+  scope = "/subscriptions/${var.subscription_id}"
+}
+
 resource "azurerm_role_assignment" "terraform_plan_state" {
   scope                = azurerm_storage_container.terraform_state.id
   role_definition_name = "Storage Blob Data Contributor"
@@ -19,11 +24,35 @@ resource "azurerm_role_assignment" "terraform_plan_project" {
   principal_type       = "ServicePrincipal"
 }
 
+resource "azurerm_role_assignment" "terraform_plan_aks_user" {
+  scope                = azurerm_resource_group.project.id
+  role_definition_name = "Azure Kubernetes Service Cluster User Role"
+  principal_id         = azuread_service_principal.terraform_plan.object_id
+  principal_type       = "ServicePrincipal"
+}
+
 resource "azurerm_role_assignment" "terraform_apply_project" {
   scope                = azurerm_resource_group.project.id
   role_definition_name = "Contributor"
   principal_id         = azuread_service_principal.terraform_apply.object_id
   principal_type       = "ServicePrincipal"
+}
+
+resource "azurerm_role_assignment" "terraform_apply_network_rbac" {
+  scope                = azurerm_resource_group.project.id
+  role_definition_name = "Role Based Access Control Administrator"
+  principal_id         = azuread_service_principal.terraform_apply.object_id
+  principal_type       = "ServicePrincipal"
+  condition_version    = "2.0"
+  condition            = <<-CONDITION
+    ((!(ActionMatches{'Microsoft.Authorization/roleAssignments/write'})) OR
+      (@Request[Microsoft.Authorization/roleAssignments:RoleDefinitionId] ForAnyOfAnyValues:GuidEquals {${basename(data.azurerm_role_definition.network_contributor.role_definition_id)}} AND
+      @Request[Microsoft.Authorization/roleAssignments:PrincipalType] ForAnyOfAnyValues:StringEqualsIgnoreCase {'ServicePrincipal'}))
+    AND
+    ((!(ActionMatches{'Microsoft.Authorization/roleAssignments/delete'})) OR
+      (@Resource[Microsoft.Authorization/roleAssignments:RoleDefinitionId] ForAnyOfAnyValues:GuidEquals {${basename(data.azurerm_role_definition.network_contributor.role_definition_id)}} AND
+      @Resource[Microsoft.Authorization/roleAssignments:PrincipalType] ForAnyOfAnyValues:StringEqualsIgnoreCase {'ServicePrincipal'}))
+  CONDITION
 }
 
 resource "azurerm_role_assignment" "aks_network" {
