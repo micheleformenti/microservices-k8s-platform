805ce8597896ceae0698eaaf42382fcff48cc37f
Michele Formenti <michele.formenti@proton.me>
2026-08-21 13:42:25 +0200

    Isolate AKS identity import

--- terraform/azure/aks/identity.tf
+++ terraform/azure/aks/identity.tf
@@ -1,4 +1,4 @@
-resource "azurerm_user_assigned_identity" "aks" {
+resource "azurerm_user_assigned_identity" "aks_imported" {
   name                = "id-${var.project_name}-aks"
   location            = data.azurerm_resource_group.project.location
   resource_group_name = data.azurerm_resource_group.project.name
@@ -6,12 +6,12 @@ resource "azurerm_user_assigned_identity" "aks" {
   tags = local.common_tags
 }
 
-data "azurerm_user_assigned_identity" "aks_existing" {
+data "azurerm_user_assigned_identity" "aks" {
   name                = "id-${var.project_name}-aks"
   resource_group_name = data.azurerm_resource_group.project.name
 }
 
 import {
-  to = azurerm_user_assigned_identity.aks
+  to = azurerm_user_assigned_identity.aks_imported
   id = "/subscriptions/${var.subscription_id}/resourceGroups/rg-${var.project_name}/providers/Microsoft.ManagedIdentity/userAssignedIdentities/id-${var.project_name}-aks"
 }
